57e5faf5c5f840ca0bd62241b02a7c6af05a3601
Liying Wang <jaybin502@gmail.com>
2020-03-30 14:54:56 -0700

    update the selected columns

--- analysis/paper/KWL_burial_pre.R
+++ analysis/paper/KWL_burial_pre.R
@@ -2,25 +2,21 @@ library(readxl)
 library(tidyverse)
 library(here)
 
-burial <- read_excel(here("index", "data", "burials", "Kiwulan_Burials.xlsx"))
+burial <- read_excel(here("analysis", "data", "raw_data", "Kiwulan_Burials.xlsx"))
 
 # combine some periods
 burial_three_period_tidy <-
-  burial %>% 
-  rename(burial_label = ID) %>% 
-  mutate(Phase = ifelse(Phase == 'euro', 'post', Phase)) %>% 
-  filter(!is.na(Phase)) %>% 
+  burial %>%
+  rename(burial_label = ID) %>%
+  mutate(Phase = ifelse(Phase == 'euro', 'post', Phase)) %>%
+  filter(!is.na(Phase)) %>%
   mutate(Gold_leaf = ifelse(Gold_leaf == "shatter", "1", Gold_leaf),
          Stoneware = ifelse(Stoneware == "base", "1", Stoneware),
-         Stamped_ceramic = ifelse(Stamped_ceramic == "cluster", "1", Stamped_ceramic)) %>% 
-  mutate(`Indo-Pacific_bead` = as.numeric(`Indo-Pacific_bead`),
-         `Glass_bead` = as.numeric(`Glass_bead`),
-         `Gold_leaf` = as.numeric(`Gold_leaf`),
-         Stoneware = as.numeric(Stoneware),
-         `Stamped_ceramic` = as.numeric(`Stamped_ceramic`)) %>% 
-  janitor::remove_empty(which = "cols") %>% 
-  mutate(total = rowSums(.[21:40], na.rm = TRUE)) %>% 
-  mutate(quantity = case_when( 
+         Stamped_ceramic = ifelse(Stamped_ceramic == "cluster", "1", Stamped_ceramic)) %>%
+  mutate_at(21:ncol(.), as.numeric) %>%
+  janitor::remove_empty(which = "cols") %>%
+  mutate(total = rowSums(.[c(21:48, 50, 55, 56)], na.rm = TRUE)) %>%
+  mutate(quantity = case_when(
     total == 0 ~ "none",
     total > 0 & total <= 7 ~ "low",
     total > 7 & total < 100 ~ "medium",
@@ -35,8 +31,8 @@ burial_three_period_tidy <-
          `Indo-Pacific_bead_low` = ifelse(`Indo-Pacific_bead` < 100, 1, NA),
          `Indo-Pacific_bead_med` = ifelse(`Indo-Pacific_bead` > 100 & `Indo-Pacific_bead` < 900, 1, NA),
          `Indo-Pacific_bead_high` = ifelse(`Indo-Pacific_bead` > 900, 1, NA)) %>% #based on the result of histogram
-  select(burial_label, 
-         Phase, 
+  select(burial_label,
+         Phase,
          Gold_bead_low,
          Gold_bead_med,
          Gold_bead_high,
@@ -56,93 +52,93 @@ burial_three_period_tidy <-
 
 # number of each phase
 burial_three_period_number <-
-  burial_three_period_tidy %>% 
+  burial_three_period_tidy %>%
   count(Phase)
 
 burial_pre <-
-  burial_three_period_tidy %>% 
+  burial_three_period_tidy %>%
   filter(Phase == "pre")
-  
+
 # create node list using burial index by phase
 #--------------------pre--------------------------------
-nodes_pre <- 
-  burial_three_period_tidy %>% 
-  filter(Phase == "pre") %>% 
-  select(burial_label) %>% 
+nodes_pre <-
+  burial_three_period_tidy %>%
+  filter(Phase == "pre") %>%
+  select(burial_label) %>%
   rowid_to_column("id")
 
 # pair wise combinations for burials as index for later map function
 library(gtools)
 burial_comb_pre = combinations(length(nodes_pre$burial_label), #t(combn(nodes_pre$burial_label, 2))
-                               2, nodes_pre$burial_label, 
+                               2, nodes_pre$burial_label,
                                repeats = TRUE)
 colnames(burial_comb_pre) = c("burial_1", "burial_2")
 burial_comb_pre = as_tibble(burial_comb_pre)
 
 # create list for each burial that contains the burial good types and their counts
 edge_list_pre <-
-  burial_three_period_tidy %>% 
+  burial_three_period_tidy %>%
   select(burial_label, 3:8) %>% #19:39
-  pivot_longer(-burial_label, names_to = "goods", values_to = "count") %>% 
+  pivot_longer(-burial_label, names_to = "goods", values_to = "count") %>%
   #mutate(burial_connection = rep(unique(burial_label), length.out = length(burial_label)))
-  group_by(burial_label) %>% 
-  nest() 
+  group_by(burial_label) %>%
+  nest()
 
-# pair-wise list 
-common_counts_lst_pre <- 
+# pair-wise list
+common_counts_lst_pre <-
 map2(burial_comb_pre$burial_1,
      burial_comb_pre$burial_2,
      ~bind_cols(
-       edge_list_pre %>% 
-       filter(burial_label == .x) %>% 
+       edge_list_pre %>%
+       filter(burial_label == .x) %>%
        unnest(data),
-       edge_list_pre %>% 
-         filter(burial_label == .y) %>% 
-         unnest(data)) %>% 
-       rowwise() %>% 
+       edge_list_pre %>%
+         filter(burial_label == .y) %>%
+         unnest(data)) %>%
+       rowwise() %>%
        mutate(common_counts = sum(count, count1)))
 
-# count of ornament types in common between each pair of burials 
+# count of ornament types in common between each pair of burials
 common_counts_vct_pre <- map_int(common_counts_lst_pre, ~sum(!is.na(.x$common_counts)))
 
-burial_comb_with_common_counts_pre <- 
-burial_comb_pre %>% 
-  mutate(common_counts = common_counts_vct_pre) %>% 
+burial_comb_with_common_counts_pre <-
+burial_comb_pre %>%
+  mutate(common_counts = common_counts_vct_pre) %>%
   mutate(common_counts = ifelse(burial_1 == burial_2, 0, common_counts)) # use 0 for self loop
 
 # change label to ids for node linking
-edges_pre <- 
-  burial_comb_with_common_counts_pre %>% 
-  left_join(nodes_pre, by = c("burial_1" = "burial_label")) %>% 
-  rename(from = id) %>% 
-  left_join(nodes_pre, by = c("burial_2" = "burial_label")) %>% 
-  rename(to = id) 
+edges_pre <-
+  burial_comb_with_common_counts_pre %>%
+  left_join(nodes_pre, by = c("burial_1" = "burial_label")) %>%
+  rename(from = id) %>%
+  left_join(nodes_pre, by = c("burial_2" = "burial_label")) %>%
+  rename(to = id)
 
-edges_for_network_pre <- 
-  select(edges_pre, from, to, common_counts) %>% 
-  filter (!common_counts == 0) 
+edges_for_network_pre <-
+  select(edges_pre, from, to, common_counts) %>%
+  filter (!common_counts == 0)
  # %>% mutate(common_counts = ifelse(common_counts > 1, 1, common_counts)) # for unweighted network
-  
+
 #1--------------------network analysis using ggraph pkg------------------------------
 library(tidygraph)
 library(ggraph)
 
-relation_tidy_pre <- tbl_graph(nodes = nodes_pre, 
-                               edges = edges_for_network_pre, 
+relation_tidy_pre <- tbl_graph(nodes = nodes_pre,
+                               edges = edges_for_network_pre,
                                directed = FALSE)
 
-relation_tidy_pre %>% 
-  activate(edges) %>% 
+relation_tidy_pre %>%
+  activate(edges) %>%
   arrange(desc(common_counts))
 
-ggraph(relation_tidy_pre) + 
-  geom_edge_link() + 
-  geom_node_point() + 
+ggraph(relation_tidy_pre) +
+  geom_edge_link() +
+  geom_node_point() +
   theme_graph()
 
-ggraph(relation_tidy_pre, layout = "graphopt") + 
+ggraph(relation_tidy_pre, layout = "graphopt") +
   geom_node_point() +
-  geom_edge_link(aes(width = common_counts), alpha = 0.8) + 
+  geom_edge_link(aes(width = common_counts), alpha = 0.8) +
   scale_edge_width(range = c(0.2, 2)) +
   geom_node_text(aes(label = burial_label), repel = TRUE) +
   labs(edge_width = "common item") +
@@ -151,11 +147,11 @@ ggraph(relation_tidy_pre, layout = "graphopt") +
 #2-------------------network analysis using network pkg-------------------------------
 library(network)
 
-burial_network_pre <- 
+burial_network_pre <-
   network(edges_for_network_pre, # the network object
           vertex.attr = nodes_pre, # node list
           directed = FALSE, # specify whether the network is directed
-          ignore.eval = TRUE, # FALSE = weighted 
+          ignore.eval = TRUE, # FALSE = weighted
           loops = FALSE, # do we allow self ties (should not allow them)
           matrix.type = "edgelist") # the type of input
 
@@ -172,14 +168,14 @@ set.vertex.attribute(burial_network_pre, "quantity", burial_pre$quantity)
 # plot
 set.seed(30)
 quantity <- get.vertex.attribute(burial_network_pre, "quantity")
-plot(burial_network_pre, 
+plot(burial_network_pre,
      vertex.col = "quantity",
      vertex.cex = 1.5)
 
-legend("topleft", 
+legend("topleft",
        col = c(3, 2, 4, 1),
-       pch    = 20, 
-       legend = unique(quantity), 
+       pch    = 20,
+       legend = unique(quantity),
        title  = 'Burial good counts')
 
 # ? can't get the items in legend in order
@@ -187,15 +183,15 @@ legend("topleft",
 # ? can't match color with categories, using an odd method at the moment
 
 #------------------creating ERGM model-------------------------------------
-# predict ties, as function of network structures, 
+# predict ties, as function of network structures,
 # endogenous statistics that ties are modelled based on the existence of other ties
 # exogenous statistics that ties are formed according to monadic or dyadic node attributes
-# Kiwulan case 
+# Kiwulan case
 # https://www.r-bloggers.com/ergm-tutorial/
 
 # every term in an ERGM must have an associated algorithm for computing its value for network
 # Morris et al. 2008
-model.ergm <- burial_network_pre ~ 
+model.ergm <- burial_network_pre ~
   edges + # ties, a measure of density, equal to kstar(1) for undirected networks
   density +
   gwdegree(0.5, fixed = TRUE)  +
@@ -250,16 +246,16 @@ parpost <- bergm(model.3,
                   nchains     = 8, # number of chains of the population MCMC
                   gamma       = 0.7) # scalar; parallel adaptive direction sampling move factor, acceptance rate
 
-summary(parpost) # Each θ corresponds to the parameter specified in ERGM previously 
+summary(parpost) # Each θ corresponds to the parameter specified in ERGM previously
 # In general, positive mean indicates postive correlation, while negative mean indicates negative correlation
 # there is different statistics between weighted and unweighted ties
 # θ1 = number of ties, θ2 = individuals with the same abundance of burial goods
 # θ3 = gwesp is negative that rejects the assumption that actors with multiple partners in common are more likely to be directed connected
-# θ4 = 
+# θ4 =
 
 plot(parpost)
 
-# estimate the parameter posterior distribution using the pseudo-posterior calibration approach 
+# estimate the parameter posterior distribution using the pseudo-posterior calibration approach
 # this will take less than the exchange algorithm
 
 parpost2 <- bergmC(model.3,
@@ -267,22 +263,22 @@ parpost2 <- bergmC(model.3,
                    prior.sigma = prior.sigma,
                    iters       = 100,
                    aux.iters   = 10000,
-                   noisy.nsim  = 1000,  
+                   noisy.nsim  = 1000,
                    noisy.thin  = 1000,
                    burnin      = 200,
                    mcmc        = 10000,
                    tunePL      = 1.5)
 
 
-bergmC(formula, 
-       prior.mean = NULL, 
+bergmC(formula,
+       prior.mean = NULL,
        prior.sigma = NULL,
-       burn.in = 10000, 
-       main.iters = 40000, 
+       burn.in = 10000,
+       main.iters = 40000,
        aux.iters = 3000,
-       V.proposal = 1.5, 
-       thin = 1, 
-       rm.iters = 500, 
+       V.proposal = 1.5,
+       thin = 1,
+       rm.iters = 500,
        rm.a = 0.001,
        rm.alpha = 0, n.aux.draws = 400, aux.thin = 50,
        estimate = c("MLE", "CD"), ...)
@@ -298,20 +294,20 @@ bgof(parpost,
 #_-------------------------set different parameters------------------------------
 # three competing models based on ERGM formulas to fit the data using gwesp and gwdegree
 
-mod.1 <- burial_network_pre ~ edges + 
+mod.1 <- burial_network_pre ~ edges +
   gwesp(0.2, fixed = TRUE)
 summary(mod.1)
 
-mod.2 <- burial_network_pre ~ edges + 
+mod.2 <- burial_network_pre ~ edges +
   gwdegree(0.8, fixed = TRUE)
 summary(mod.2)
 
-mod.3 <- burial_network_pre ~ edges + 
-  gwesp(0.2, fixed = TRUE) + 
+mod.3 <- burial_network_pre ~ edges +
+  gwesp(0.2, fixed = TRUE) +
   gwdegree(0.8, fixed = TRUE)
 summary(mod.3)
 
-mod.sel <- bergm(mod.1, 
+mod.sel <- bergm(mod.1,
                  prior.mean  = NULL,
                  prior.sigma = NULL,
                  aux.iters = 100, # auxiliary iterations used for network simulation
@@ -323,8 +319,8 @@ mod.sel <- bergm(mod.1,
 summary(parpost)
 plot(parpost)
 
-mod.sel <- bergm(mod.1, iters = 25000, 
+mod.sel <- bergm(mod.1, iters = 25000,
                  aux.iters = 2000,
-                 main.iters = rep(700, 3), 
-                 burn.ins = rep(100, 3), 
+                 main.iters = rep(700, 3),
+                 burn.ins = rep(100, 3),
                  gammas = c(1, 1, 0.8))
--- analysis/paper/burial_class_plot.R
+++ analysis/paper/burial_class_plot.R
@@ -1,57 +1,51 @@
 # tidy up
 burial_three_period_tidy_explore <-
-  burial %>% 
-  rename(burial_label = ID) %>% 
-  mutate(Phase = ifelse(Phase == 'euro', 'post', Phase)) %>% 
-  filter(!is.na(Phase)) %>% 
+  burial %>%
+  rename(burial_label = ID) %>%
+  mutate(Phase = ifelse(Phase == 'euro', 'post', Phase)) %>%
+  filter(!is.na(Phase)) %>%
   mutate(Gold_leaf = ifelse(Gold_leaf == "shatter", "1", Gold_leaf),
          Stoneware = ifelse(Stoneware == "base", "1", Stoneware),
-         Stamped_ceramic = ifelse(Stamped_ceramic == "cluster", "1", Stamped_ceramic)) %>% 
-  mutate(`Indo-Pacific_bead` = as.numeric(`Indo-Pacific_bead`),
-         `Gold_leaf` = as.numeric(`Gold_leaf`),
-         Stoneware = as.numeric(Stoneware),
-         `Stamped_ceramic` = as.numeric(`Stamped_ceramic`)) %>% 
-  janitor::remove_empty(which = "cols") %>% 
-  mutate(total = rowSums(.[19:39], na.rm = TRUE)) %>% 
-  mutate(quantity = case_when( 
+         Stamped_ceramic = ifelse(Stamped_ceramic == "cluster", "1", Stamped_ceramic)) %>%
+  mutate_at(21:ncol(.), as.numeric) %>%
+  janitor::remove_empty(which = "cols") %>%
+  mutate(total = rowSums(.[c(21:48, 50, 55, 56)], na.rm = TRUE)) %>%
+  mutate(quantity = case_when(
     total == 0 ~ "none",
     total > 0 & total <= 7 ~ "low",
     total > 7 & total < 100 ~ "medium",
     total >= 100 ~ "high",
-    TRUE ~ "other")) %>% # the classification is based on the result of histogram
-  mutate(Gold_bead_low = ifelse(Golden_bead == 1, 1, NA),
-         Gold_bead_med = ifelse(Golden_bead > 1 & Golden_bead <10, 1, NA),
-         Gold_bead_high = ifelse(Golden_bead > 10, 1, NA)) 
+    TRUE ~ "other"))  # the classification is based on the result of histogram
 
 # Histogram to represent the count of burial goods
-draw_lines <- 
-  burial_three_period_tidy_explore %>% 
-  group_by(quantity) %>% 
-  summarise(max = min(total)) %>% 
+draw_lines <-
+  burial_three_period_tidy_explore %>%
+  group_by(quantity) %>%
+  summarise(max = min(total)) %>%
   filter(max != 0)
 
 # total burial goods
-burial_three_period_tidy_explore %>% 
+burial_three_period_tidy_explore %>%
   ggplot(aes(total)) +
   geom_histogram() +
   geom_vline(xintercept = draw_lines$max,
              colour = "red") +
-  scale_x_log10() 
+  scale_x_log10()
 
 # gold bead
-burial_three_period_tidy_explore %>% 
+burial_three_period_tidy_explore %>%
   ggplot(aes(Golden_bead)) +
   geom_histogram() +
-  scale_x_log10() 
+  scale_x_log10()
 
 # agate bead
-burial_three_period_tidy_explore %>% 
+burial_three_period_tidy_explore %>%
   ggplot(aes(Agate_bead)) +
   geom_histogram() +
-  scale_x_log10() 
+  scale_x_log10()
 
 # glass bead
-burial_three_period_tidy_explore %>% 
+burial_three_period_tidy_explore %>%
   ggplot(aes(`Indo-Pacific_bead`)) +
   geom_histogram() +
-  scale_x_log10() 
+  scale_x_log10()
