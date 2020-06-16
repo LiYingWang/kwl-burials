54da4309f38018622a3523a1cfe24293d91305c1
Liying Wang <jaybin502@gmail.com>
2020-06-15 23:48:31 -0900

    work on method and reorganize paragraphs

---
+++ R/paper_note.R
@@ -0,0 +1,9 @@
+1. edges: ties, a measure of density, equal to kstar(1) for undirected networks
+
+2. gwesp (geometrically weighted edgewise shared partners): triad-based clustering based on a dyad-based configurations (not node-based) that counts the number of times that both nodes have a tie to a third node. It is a measure of the tendency for two actors who are tied to have x partners in common. In other words, a tendency for those with shared partners to become tied, or tendency of ties to cluster together.
+
+3. gwnsp (geometrically weighted non-edgewise shared partner statistics): a function of the non-edgewise shared partner statistics. The edgewise and non-edgewise shared partner statistics reflect tendencies toward transitivity and compared together would give a more comprehensive result.
+
+4. gwdegree (geometrically weighted degree distribution): a parametric form that represents the frequency distribution for nodal degree, each node counts only once. The local configuration is degrees and stars that are equivalent representations for the distribution of node-based edge counts. A measure of the tendency of the number of edges into a node. This statistic captures the tendency towards centralisation in the degree distribution of the network.
+
+5. nodematch: uniform homophily between nodes that refers to the similarity of connected nodes. A statistics that counts the number of edges between nodes sharing the same covariate value or characteristics. We use wealth index and age as covariants that reflects the similarity between nodes. Here we consider age, gender, and index of wealth as node attributes covariants.
