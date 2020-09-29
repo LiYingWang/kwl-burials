99e11a84ba2d7d2decb5760726dc7e179983494e
Liying Wang <jaybin502@gmail.com>
2020-09-29 04:50:45 -0700

    modify the burial map

--- analysis/paper/002-burial-location.R
+++ analysis/paper/002-burial-location.R
@@ -12,18 +12,28 @@ burial_three_period_age_tidy_geo <-
   burial_shape %>%
   left_join(burial_three_period_age_tidy,
             by = c("No_burial" = "burial_label")) %>%
-  mutate(burial_cent = st_centroid(geometry))
+  mutate(burial_cent = st_centroid(geometry)) %>%
+  mutate(Period = case_when(
+    Phase.y == "pre" ~ "Pre-European",
+    Phase.y == "post" ~ "Pos-European",
+    Phase.y == "chi" ~ "Chinese",
+    TRUE ~ ""))
 
 # mapping burials over the AD section
 KWL_burial_map <-
   burial_three_period_age_tidy_geo %>%
   filter(!is.na(geometry)) %>%
+  filter(Phase.y != "disturbed") %>%
   ggplot() +
-  geom_sf(aes(fill = Phase.y)) +
+  geom_sf(aes(fill = Period)) +
   geom_sf(data = post, color = 'blue', size = 0.2, alpha = 0.4) +
   geom_sf(data = AD_data, fill = NA) +
   #geom_sf_text(data = AD_data, aes(label = `坑號`), size = 2.5,
                #alpha = 0.3, fontface = "bold") +
+  scale_fill_viridis_d() +
   theme_minimal() +
   blank() +
   north(AD_data, scale = 0.2, symbol = 3)
+
+distances <-
+  st_distance(burial_three_period_age_tidy_geo$burial_cent)
