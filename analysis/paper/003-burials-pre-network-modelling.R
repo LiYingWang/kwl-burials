89a9c41d921bb61b487b68786a88024e3bf48bc8
Liying Wang <jaybin502@gmail.com>
2020-12-09 00:00:15 -0800

    udpate discussion section

--- analysis/paper/003-burials-pre-network-modelling.R
+++ analysis/paper/003-burials-pre-network-modelling.R
@@ -197,9 +197,9 @@ summary(model_pre_3)
 
 #--------------------Bayesian inference for ERGMs-------------------------
 # prior suggestion: normal distribution (low density and high transitivity), but it also depends on the ERGM netowrk we observed
-prior.mean <- c(-5, 0, 0, 1, 0, 2, -1, -1) # positive prior number for edge means high density
+prior.mean <- c(-3, 0, 0, 1, 0, 2, -1, -1) # positive prior number for edge means high density
 # follow Alberto Caimo et al. (2015) hospital example
-prior.sigma <- diag(c(5, 3, 3, 5, 3, 5, 3, 3), 8, 8) # covariance matrix structure, uncertainty
+prior.sigma <- diag(c(4, 5, 3, 5, 5, 5, 2, 3), 8, 8) # covariance matrix structure, uncertainty
 
 # normal distribution 𝜃 ∼ Nd (𝜇prior , Σprior ) a common prior model
 # where the dimension d corresponds to the number of parameters, 𝜇 is mean vector and Σprior is a d × d covariance matrix.
