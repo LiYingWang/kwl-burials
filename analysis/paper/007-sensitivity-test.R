e7c0c36dcf0d9781fe0804a23cd1d0495f19da14
Liying Wang <jaybin502@gmail.com>
2020-12-15 23:18:02 -0800

    compute CIs for post-E network stats

--- analysis/paper/007-sensitivity-test.R
+++ analysis/paper/007-sensitivity-test.R
@@ -1,7 +1,7 @@
 # run 006-bergm-bootstrap
 #------------------------functions for network stats---------------------------
 # function to calculate density using vertex bootstrap
-boot_density <- function(igraph_net) {
+boot_density <- function(pre_rm_matrix) {
   rm_matrix <- as.matrix(as_adjacency_matrix(igraph_net))
   boot <- 1000
   set.seed(1)
@@ -9,17 +9,6 @@ boot_density <- function(igraph_net) {
   pre_5_rm_boot_density <- sapply(1:boot, function(x)
     graph.density(graph_from_adjacency_matrix(pre_5_rm_Astar[[x]])))
   return(pre_5_rm_boot_density)
-}
-
-# function to calculate degree using vertex bootstrap
-boot_degree <- function(igraph_net) {
-  rm_matrix <- as.matrix(as_adjacency_matrix(igraph_net))
-  boot <- 1000
-  set.seed(1)
-  pre_5_rm_Astar <- vertboot(rm_matrix, boot)
-  pre_5_rm_boot_degree <- sapply(1:boot, function(x)
-    mean(degree(graph_from_adjacency_matrix(pre_5_rm_Astar[[x]]), mode = "in")))
-  return(pre_5_rm_boot_degree)
 }
 
 # function to calculate transitivity using vertex bootstrap
@@ -33,12 +22,31 @@ boot_trans <- function(igraph_net) {
   return(pre_5_rm_boot_trans)
 }
 
+# function to calculate degree using vertex bootstrap
+boot_degree <- function(igraph_net) {
+  rm_matrix <- as.matrix(as_adjacency_matrix(igraph_net))
+  boot <- 1000
+  set.seed(1)
+  pre_5_rm_Astar <- vertboot(rm_matrix, boot)
+  pre_5_rm_boot_degree <- sapply(1:boot, function(x)
+    mean(degree(graph_from_adjacency_matrix(pre_5_rm_Astar[[x]]), mode = "in")))
+  return(pre_5_rm_boot_degree)
+}
+
 # function for getting stats from observed network
 obs_stats_all <- function(igraph_net) {
   obs_stats <- rbind(graph.density(igraph_net),
-                     mean(degree(igraph_net)),
-                     transitivity(igraph_net))
+                     transitivity(igraph_net),
+                     mean(degree(igraph_net)))
   return(obs_stats)
+}
+
+# function for combine 95% CI of three stats and observations
+three_CI_and_obs <- function(net_after_node_removal) {
+  as.data.frame(rbind(quantile(boot_density(net_after_node_removal), c(0.025, 0.975)),
+                      quantile(boot_trans(net_after_node_removal), c(0.025, 0.975)),
+                      quantile(boot_degree(net_after_node_removal), c(0.025, 0.975)))) %>%
+    cbind(obs = obs_stats_all(net_after_node_removal))
 }
 
 #-----------------------vertex removal for pre-E-------------------------------------
@@ -48,17 +56,8 @@ pre_5_node_removal <-
                                        round(nrow(nodes_pre)*0.05),
                                        replace=F)) # remove 1 nodes
 
-# vertex bootstrap for density, degree, and transitivity
-pre_5_rm_boot_density <- boot_density(pre_5_node_removal)
-pre_5_rm_boot_star <- boot_degree(pre_5_node_removal)
-pre_5_rm_boot_trans <- boot_trans(pre_5_node_removal)
-
-# combine 95% CI for three stats and observations
-pre_5_rm_boot_all_stats <-
-  as.data.frame(rbind(quantile(pre_5_rm_boot_density, c(0.025, 0.975)),
-                      quantile(pre_5_rm_boot_star, c(0.025, 0.975)),
-                      quantile(pre_5_rm_boot_trans, c(0.025, 0.975)))) %>%
-  cbind(obs = obs_stats_all(pre_5_node_removal))
+# get 95% CI for three stats and observations
+pre_5_rm_boot_all_stats <- three_CI_and_obs(pre_5_node_removal)
 
 # remove 10% nodes (3 nodes) based on network with 5% nodes removal
 pre_10_node_removal <-
@@ -67,136 +66,233 @@ pre_10_node_removal <-
                          round(nrow(nodes_pre)*0.10-round(nrow(nodes_pre)*0.05)),
                          replace=F)) # remove 3 nodes
 
-# vertex bootstrap for density, degree, and transitivity
-pre_10_rm_boot_density <- boot_density(pre_10_node_removal)
-pre_10_rm_boot_star <- boot_degree(pre_10_node_removal)
-pre_10_rm_boot_trans <- boot_trans(pre_10_node_removal)
-
-# combine 95% CI for three stats and observations
-pre_10_rm_boot_all_stats <-
-  as.data.frame(rbind(quantile(pre_10_rm_boot_density, c(0.025, 0.975)),
-                      quantile(pre_10_rm_boot_star, c(0.025, 0.975)),
-                      quantile(pre_10_rm_boot_trans, c(0.025, 0.975)))) %>%
-  cbind(obs = obs_stats_all(pre_10_node_removal))
+# get 95% CI for three stats and observations
+pre_10_rm_boot_all_stats <- three_CI_and_obs(pre_10_node_removal)
 
 # remove 15% nodes (4 nodes) based on network with 10% nodes removal
 pre_15_node_removal <-
   delete_vertices(pre_10_node_removal,
                   sample(1:(nrow(nodes_pre)-round(nrow(nodes_pre)*0.10)),
                          round(nrow(nodes_pre)*0.15-round(nrow(nodes_pre)*0.10)),
-                         replace=F)) # remove 3 nodes
+                         replace=F))
 
-# vertex bootstrap for density, degree, and transitivity
-pre_15_rm_boot_density <- boot_density(pre_15_node_removal)
-pre_15_rm_boot_star <- boot_degree(pre_15_node_removal)
-pre_15_rm_boot_trans <- boot_trans(pre_15_node_removal)
-
-# combine 95% CI for three stats and observations
-pre_15_rm_boot_all_stats <-
-  as.data.frame(rbind(quantile(pre_15_rm_boot_density, c(0.025, 0.975)),
-                      quantile(pre_15_rm_boot_star, c(0.025, 0.975)),
-                      quantile(pre_15_rm_boot_trans, c(0.025, 0.975)))) %>%
-  cbind(obs = obs_stats_all(pre_15_node_removal))
+# get 95% CI for three stats and observations
+pre_15_rm_boot_all_stats <- three_CI_and_obs(pre_15_node_removal)
 
 # remove 20% nodes (6 nodes) based on network with 15% nodes removal
 pre_20_node_removal <-
   delete_vertices(pre_15_node_removal,
                   sample(1:(nrow(nodes_pre)-round(nrow(nodes_pre)*0.15)),
                          round(nrow(nodes_pre)*0.20-round(nrow(nodes_pre)*0.15)),
-                         replace=F)) # remove 3 nodes
+                         replace=F))
 
-# vertex bootstrap for density, degree, and transitivity
-pre_20_rm_boot_density <- boot_density(pre_20_node_removal)
-pre_20_rm_boot_star <- boot_degree(pre_20_node_removal)
-pre_20_rm_boot_trans <- boot_trans(pre_20_node_removal)
-
-# combine 95% CI for three stats and observations
-pre_20_rm_boot_all_stats <-
-  as.data.frame(rbind(quantile(pre_20_rm_boot_density, c(0.025, 0.975)),
-                      quantile(pre_20_rm_boot_star, c(0.025, 0.975)),
-                      quantile(pre_20_rm_boot_trans, c(0.025, 0.975)))) %>%
-  cbind(obs = obs_stats_all(pre_20_node_removal))
+# get 95% CI for three stats and observations
+pre_20_rm_boot_all_stats <- three_CI_and_obs(pre_20_node_removal)
 
 # remove 25% nodes (7 nodes) based on network with 20% nodes removal
 pre_25_node_removal <-
   delete_vertices(pre_20_node_removal,
                   sample(1:(nrow(nodes_pre)-round(nrow(nodes_pre)*0.20)),
                          round(nrow(nodes_pre)*0.25-round(nrow(nodes_pre)*0.20)),
-                         replace=F)) # remove 3 nodes
+                         replace=F))
 
-# vertex bootstrap for density, degree, and transitivity
-pre_25_rm_boot_density <- boot_density(pre_25_node_removal)
-pre_25_rm_boot_star <- boot_degree(pre_25_node_removal)
-pre_25_rm_boot_trans <- boot_trans(pre_25_node_removal)
+# get 95% CI for three stats and observations
+pre_25_rm_boot_all_stats <- three_CI_and_obs(pre_25_node_removal)
 
-# combine 95% CI for three stats and observations
-pre_25_rm_boot_all_stats <-
-  as.data.frame(rbind(quantile(pre_25_rm_boot_density, c(0.025, 0.975)),
-                      quantile(pre_25_rm_boot_star, c(0.025, 0.975)),
-                      quantile(pre_25_rm_boot_trans, c(0.025, 0.975)))) %>%
-  cbind(obs = obs_stats_all(pre_25_node_removal))
+# remove 30% nodes (9 nodes) based on network with 25% nodes removal
+pre_30_node_removal <-
+  delete_vertices(pre_25_node_removal,
+                  sample(1:(nrow(nodes_pre)-round(nrow(nodes_pre)*0.25)),
+                         round(nrow(nodes_pre)*0.30-round(nrow(nodes_pre)*0.25)),
+                         replace=F))
+
+# get 95% CI for three stats and observations
+pre_30_rm_boot_all_stats <- three_CI_and_obs(pre_30_node_removal)
+
+# remove 35% nodes (10 nodes) based on network with 30% nodes removal
+pre_35_node_removal <-
+  delete_vertices(pre_30_node_removal,
+                  sample(1:(nrow(nodes_pre)-round(nrow(nodes_pre)*0.30)),
+                         round(nrow(nodes_pre)*0.35-round(nrow(nodes_pre)*0.30)),
+                         replace=F))
+
+# get 95% CI for three stats and observations
+pre_35_rm_boot_all_stats <- three_CI_and_obs(pre_35_node_removal)
+
+# remove 40% nodes (12 nodes) based on network with 35% nodes removal
+pre_40_node_removal <-
+  delete_vertices(pre_35_node_removal,
+                  sample(1:(nrow(nodes_pre)-round(nrow(nodes_pre)*0.35)),
+                         round(nrow(nodes_pre)*0.40-round(nrow(nodes_pre)*0.35)),
+                         replace=F))
+
+# get 95% CI for three stats and observations
+pre_40_rm_boot_all_stats <- three_CI_and_obs(pre_40_node_removal)
 
 #-----------------------coverage probabilities-------------------------------------
 # coverage probabilities
-cov_5_mean_all <- rbind(mean(pre_5_rm_boot_density),
-                        mean(pre_5_rm_boot_star),
-                        mean(pre_5_rm_boot_trans))
+pre_cov_5_mean_all <- rbind(mean(boot_density(pre_5_node_removal)),
+                        mean(boot_trans(pre_5_node_removal)),
+                        mean(boot_degree(pre_5_node_removal)))
 
-cov_5_se_all <- rbind(sd(pre_5_rm_boot_density),
-                      sd(pre_5_rm_boot_star),
-                      sd(pre_5_rm_boot_trans))
+pre_cov_5_se_all <- rbind(sd(boot_density(pre_5_node_removal)),
+                      sd(boot_trans(pre_5_node_removal)),
+                      sd(boot_degree(pre_5_node_removal)))
 
-cov_5_den <- sum(pre_5_rm_boot_density <= pre_CIvertboot_den[[2]] &
-                   pre_5_rm_boot_density >= pre_CIvertboot_den[[1]])/1000
-cov_5_star <- sum(pre_5_rm_boot_star <= pre_CIvertboot_star[[2]] &
-                    pre_5_rm_boot_star >= pre_CIvertboot_star[[1]])/1000
-cov_5_trans <-sum(pre_5_rm_boot_trans <= pre_CIvertboot_trans[[2]] &
-                    pre_5_rm_boot_trans >= pre_CIvertboot_trans[[1]])/1000
+pre_cov_5_den <- sum(boot_density(pre_5_node_removal) <= pre_CIvertboot_den[[2]] &
+                   boot_density(pre_5_node_removal) >= pre_CIvertboot_den[[1]])/1000
+pre_cov_5_trans <- sum(boot_trans(pre_5_node_removal) <= pre_CIvertboot_trans[[2]] &
+                    boot_trans(pre_5_node_removal) >= pre_CIvertboot_trans[[1]])/1000
+pre_cov_5_star <-sum(boot_degree(pre_5_node_removal) <= pre_CIvertboot_star[[2]] &
+                    boot_degree(pre_5_node_removal) >= pre_CIvertboot_star[[1]])/1000
 
 pre_5_rm_cov_stats <-
-  as.data.frame(rbind(cov_5_den,
-                      cov_5_star,
-                      cov_5_trans)) %>%
-  cbind(mean = cov_5_mean_all,
-        se = cov_5_se_all) %>%
-  rename(coverage=V1)
+  as.data.frame(rbind(pre_cov_5_den,
+                      pre_cov_5_trans,
+                      pre_cov_5_star)) %>%
+  cbind(mean = pre_cov_5_mean_all,
+        se = pre_cov_5_se_all) %>%
+  rename(coverage = V1)
 
 #-----------------------vertex removal for post-E-------------------------------------
-post_E_node_removal <-
-  delete_vertices(post_E_igraph, sample(1:49, 20, replace=F)) # remove 20 nodes
+# remove 5% node (2 node)
+post_5_node_removal <-
+  delete_vertices(post_E_igraph, sample(1:nrow(nodes_post),
+                                       round(nrow(nodes_post)*0.05),
+                                       replace=F)) # remove 2 nodes
 
-post_rm_density_obs <- graph.density(post_E_node_removal)
-post_rm_mean_star_obs <- mean(degree(post_E_node_removal))
-post_rm_trans_obs <- transitivity(post_E_node_removal)
-post_rm_all_obs <- rbind(post_rm_density_obs,
-                         post_rm_mean_star_obs,
-                         post_rm_trans_obs)
-post_rm_matrix <- as.matrix(as_adjacency_matrix(post_E_node_removal))
+# get 95% CI for three stats and observations
+post_5_rm_boot_all_stats <- three_CI_and_obs(post_5_node_removal)
 
-# vertex bootstrap, useful for small network
-post_number <- 1000
-set.seed(1)
-post_rm_Astar <- vertboot(post_rm_matrix, post_number)
+# remove 10% nodes (5 nodes) based on network with 5% nodes removal
+post_10_node_removal <-
+  delete_vertices(post_5_node_removal,
+                  sample(1:(nrow(nodes_post)-round(nrow(nodes_post)*0.05)),
+                         round(nrow(nodes_post)*0.10-round(nrow(nodes_post)*0.05)),
+                         replace=F)) # remove 5 nodes
 
-# calculate 95% bootstrap confidence intervals for the density and bootstrap standard error
-post_rm_boot_density <- sapply(1:post_number, function(x)
-  graph.density(graph_from_adjacency_matrix(post_rm_Astar[[x]])))
-post_rm_CIvertboot_den <- quantile(post_rm_boot_density, c(0.025, 0.975))
-post_rm_CIvertboot_den
-post_rm_bootstrap_se <- sd(post_rm_boot_density)
-post_rm_bootstrap_se
+# get 95% CI for three stats and observations
+post_10_rm_boot_all_stats <- three_CI_and_obs(post_10_node_removal)
 
-# confedence interval for mean degree and transitivity
-post_rm_boot_star <- sapply(1:number, function(x)
-  mean(degree(graph_from_adjacency_matrix(post_rm_Astar[[x]]), mode = "in")))
-post_rm_CIvertboot_star <- quantile(post_rm_boot_star , c(0.025, 0.975))
+# remove 15% nodes (7 nodes) based on network with 10% nodes removal
+post_15_node_removal <-
+  delete_vertices(post_10_node_removal,
+                  sample(1:(nrow(nodes_post)-round(nrow(nodes_post)*0.10)),
+                         round(nrow(nodes_post)*0.15-round(nrow(nodes_post)*0.10)),
+                         replace=F))
 
-post_rm_boot_trans <- sapply(1:number, function(x)
-  transitivity(graph_from_adjacency_matrix(post_rm_Astar[[x]]), type = "undirected"))
-post_rm_CIvertboot_trans <- quantile(post_rm_boot_trans , c(0.025, 0.975))
+# get 95% CI for three stats and observations
+post_15_rm_boot_all_stats <- three_CI_and_obs(post_15_node_removal)
 
-post_rm_boot_all_stats <-
-  as.data.frame(rbind(post_rm_CIvertboot_den,
-                      post_rm_CIvertboot_star,
-                      post_rm_CIvertboot_trans)) %>%
-  cbind(obs = post_rm_all_obs)
+# remove 20% nodes (10 nodes) based on network with 15% nodes removal
+post_20_node_removal <-
+  delete_vertices(post_15_node_removal,
+                  sample(1:(nrow(nodes_post)-round(nrow(nodes_post)*0.15)),
+                         round(nrow(nodes_post)*0.20-round(nrow(nodes_post)*0.15)),
+                         replace=F))
+
+# get 95% CI for three stats and observations
+post_20_rm_boot_all_stats <- three_CI_and_obs(post_20_node_removal)
+
+# remove 25% nodes (12 nodes) based on network with 20% nodes removal
+post_25_node_removal <-
+  delete_vertices(post_20_node_removal,
+                  sample(1:(nrow(nodes_post)-round(nrow(nodes_post)*0.20)),
+                         round(nrow(nodes_post)*0.25-round(nrow(nodes_post)*0.20)),
+                         replace=F))
+
+# get 95% CI for three stats and observations
+post_25_rm_boot_all_stats <- three_CI_and_obs(post_25_node_removal)
+
+# remove 30% nodes (15 nodes) based on network with 25% nodes removal
+post_30_node_removal <-
+  delete_vertices(post_25_node_removal,
+                  sample(1:(nrow(nodes_post)-round(nrow(nodes_post)*0.25)),
+                         round(nrow(nodes_post)*0.30-round(nrow(nodes_post)*0.25)),
+                         replace=F)) # remove 3 nodes
+
+# get 95% CI for three stats and observations
+post_30_rm_boot_all_stats <- three_CI_and_obs(post_30_node_removal)
+
+# remove 35% nodes (17 nodes) based on network with 30% nodes removal
+post_35_node_removal <-
+  delete_vertices(post_30_node_removal,
+                  sample(1:(nrow(nodes_post)-round(nrow(nodes_post)*0.30)),
+                         round(nrow(nodes_post)*0.35-round(nrow(nodes_post)*0.30)),
+                         replace=F)) # remove 3 nodes
+
+# get 95% CI for three stats and observations
+post_35_rm_boot_all_stats <- three_CI_and_obs(post_35_node_removal)
+
+# remove 40% nodes (20 nodes) based on network with 35% nodes removal
+post_40_node_removal <-
+  delete_vertices(post_35_node_removal,
+                  sample(1:(nrow(nodes_post)-round(nrow(nodes_post)*0.35)),
+                         round(nrow(nodes_post)*0.40-round(nrow(nodes_post)*0.35)),
+                         replace=F)) # remove 3 nodes
+
+# get 95% CI for three stats and observations
+post_40_rm_boot_all_stats <- three_CI_and_obs(post_40_node_removal)
+
+#-----------------------coverage probabilities-------------------------------------
+# coverage probabilities
+post_cov_5_mean_all <- rbind(mean(boot_density(post_5_node_removal)),
+                        mean(boot_trans(post_5_node_removal)),
+                        mean(boot_degree(post_5_node_removal)))
+
+post_cov_5_se_all <- rbind(sd(boot_density(post_5_node_removal)),
+                      sd(boot_trans(post_5_node_removal)),
+                      sd(boot_degree(post_5_node_removal)))
+
+post_cov_5_den <- sum(boot_density(post_5_node_removal) <= post_CIvertboot_den[[2]] &
+                   boot_density(post_5_node_removal) >= post_CIvertboot_den[[1]])/1000
+post_cov_5_trans <- sum(boot_trans(post_5_node_removal) <= post_CIvertboot_trans[[2]] &
+                     boot_trans(post_5_node_removal) >= post_CIvertboot_trans[[1]])/1000
+post_cov_5_star <-sum(boot_degree(post_5_node_removal) <= post_CIvertboot_star[[2]] &
+                   boot_degree(post_5_node_removal) >= post_CIvertboot_star[[1]])/1000
+
+post_5_rm_cov_stats <-
+  as.data.frame(rbind(post_cov_5_den,
+                      post_cov_5_trans,
+                      post_cov_5_star)) %>%
+  cbind(mean = post_cov_5_mean_all,
+        se = post_cov_5_se_all) %>%
+  rename(coverage = V1)
+
+#---------------------------network stats visualization-----------------------------------
+# combines all network stats in one dataframe
+CIs_two_nets <-
+  as.data.frame(rbind(pre_5 = pre_5_rm_boot_all_stats,
+                      pre_10 = pre_10_rm_boot_all_stats,
+                      pre_15 = pre_15_rm_boot_all_stats,
+                      pre_20 = pre_20_rm_boot_all_stats,
+                      pre_25 = pre_25_rm_boot_all_stats,
+                      pre_30 = pre_30_rm_boot_all_stats,
+                      pre_35 = pre_35_rm_boot_all_stats,
+                      pre_40 = pre_40_rm_boot_all_stats,
+                      post_5 = post_5_rm_boot_all_stats,
+                      post_10 = post_10_rm_boot_all_stats,
+                      post_15 = post_15_rm_boot_all_stats,
+                      post_20 = post_20_rm_boot_all_stats,
+                      post_25 = post_25_rm_boot_all_stats,
+                      post_30 = post_30_rm_boot_all_stats,
+                      post_35 = post_35_rm_boot_all_stats,
+                      post_40 = post_40_rm_boot_all_stats))
+
+CIs_two_nets$name <- row.names(CIs_two_nets)
+
+CIs_two_nets_tidy <-
+  CIs_two_nets %>%
+  separate(name, c("phase", "node_net"), sep= "_", remove = T) %>%
+  separate(node_net, c("removal", "variable"), sep= "\\.") %>%
+  mutate(variable = case_when(
+    variable == "1" ~ "density",
+    variable == "2" ~ "transitivity",
+    variable == "3" ~ "popularity",
+    TRUE ~ "")) %>%
+  mutate(removal = as.factor(removal)) %>%
+  pivot_longer(cols = c(`2.5%`, `97.5%`),
+               names_to = "network",
+               values_to = "value")
+
+
